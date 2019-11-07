2efc28341fefdac6462eb68d9ca3b07c9bfd30dd
Remia <1808329@brunel.ac.uk>
2019-11-07 15:11:49 +0000

    Part 111a + bonus code generates the random questions and can add a new icebreaker rule

--- app/src/main/java/com/example/diceroller/DisplayMessageActivity.java
+++ app/src/main/java/com/example/diceroller/DisplayMessageActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -21,14 +22,23 @@ Editable UserInput;
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+//        textView.set
+
+    }
+
+    public void sendMessageSave(View view) {
+        EditText NewIcebreakerInput = findViewById(R.id.UserInput);
+        String s = NewIcebreakerInput.getText().toString();
+        MainActivity.stringArrayList.add(s);
+
+        // Do something in response to button
+        Intent intent = new Intent(this, MainActivity.class);//Intent takes 2 parameters Context and Class The DisplayMessage is the class that shows the new creen
+        //Context is used because aActivity is a subclass of Context
+        startActivity(intent);//opens the activity The startActivity() method starts an instance of the DisplayMessageActivity that's specified by the Intent
 
 
     }
-    public static void on_click_button(View view){
-
-
-    }
-    public void sendMessageSaveAndCancel(View view) {
+    public void sendMessageCancel(View view){
         // Do something in response to button
         Intent intent = new Intent(this, MainActivity.class);//Intent takes 2 parameters Context and Class The DisplayMessage is the class that shows the new creen
         //Context is used because aActivity is a subclass of Context
